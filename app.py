cd08cb812199360155170df1598a7ce836aaf95c
Dipti Bagal <52457642+BagalDipti@users.noreply.github.com>
2021-04-22 14:55:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template
 import socket
+import getpass
+import os.path
+import uuid
+
 
 app = Flask(__name__)
 
@@ -8,11 +12,17 @@ def index():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
+        host = socket.gethostbyaddr(socket.gethostname())
+        user_name = getpass.getuser()
+        homedir = os.path.expanduser("~")
+        Uuid=hex(uuid.getnode())
+
+        return render_template('index.html', hostname=host_name, ip=host_ip, host=host, username=user_name, homepath=homedir, Uuid=Uuid )
     except:
         return render_template('error.html')
 
 
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5678)
